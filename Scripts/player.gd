4b117a3d041beaa0f37ed285e400bcad252f3dc6
Dnmastr <89884510+Dnmastr@users.noreply.github.com>
2026-03-02 15:24:53 -0300

    atualizar mundo

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,19 +1,26 @@
 extends CharacterBody2D
 
-var speed = 100
+const SPEED = 350.0
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
-	velocity.x = 0
-	velocity.y = 0
-	
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= speed
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += speed
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += speed
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= speed
-	
-	move_and_collide(velocity * delta)
+	if direction != Vector2.ZERO:
+		velocity = direction * SPEED
+		# Troca a animação baseada na direção predominante
+		if abs(direction.x) > abs(direction.y):
+			if direction.x > 0:
+				animated_sprite.play("direita")
+			else:
+				animated_sprite.play("esquerda")
+		else:
+			if direction.y > 0:
+				animated_sprite.play("baixo")
+			else:
+				animated_sprite.play("cima")
+	else:
+		velocity = Vector2.ZERO
+		animated_sprite.play("idle") # Animação parado
+
+	move_and_slide()
