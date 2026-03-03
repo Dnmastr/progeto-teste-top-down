18c022f8844a835121749fcee983c761d0118f56
Dnmastr <89884510+Dnmastr@users.noreply.github.com>
2026-03-03 20:05:41 -0300

    pequenos detalhes

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 @onready var animated_sprite = $AnimatedSprite2D
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
 	if direction != Vector2.ZERO:
@@ -24,8 +24,3 @@ func _physics_process(delta):
 		animated_sprite.play("idle") # Animação parado
 
 	move_and_slide()
-
-func _input(event):
-	if event.is_action_pressed("ui_accept"): # Enter
-		var item = preload("res://Resources/semente_trigo.tres")
-		Inventory.adicionar_item(item)
